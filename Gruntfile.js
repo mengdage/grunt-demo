edc89e37724003fb7042da87f6362718f44564ac
meng <wulalameng@gmail.com>
2017-05-25 20:12:40 -0400

    grunt sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,22 @@
 module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
+  grunt.initConfig({
+    sass: {
+      dist: {
+        options: {
+          sourcemap: 'none'
+        },
+        files: {
+          'dist/css/style.css': 'src/sass/style.scss'
+        }
+
+      }
+    }
+  });
+
+  grunt.registerTask('default', [
+    'sass'
+  ]);
 
 };
